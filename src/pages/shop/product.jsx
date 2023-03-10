b4178fe7a814bedacb0a4eb722755c6ea6e057ed
William Flores <floreswilliam891@hotmail.com>
2023-03-09 20:27:54 -0500

    add cart number shows on shop page

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -3,7 +3,10 @@ import { ShopContext } from '../../context/shopContext'
 
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
-    const { addToCart } = useContext(ShopContext)
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemsAmount = cartItems[id]
+
     return (
     <div className='product'>
         <img src={productImage}/>
@@ -13,7 +16,9 @@ export const Product = (props) => {
                 <p>${price}</p>
             </p>
         </div>
-        <button className='addToCartBtn' onClick={() => addToCart(id)}>Add To Cart</button>
+        <button className='addToCartBtn' onClick={() => addToCart(id)}>
+            Add To Cart {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
+        </button>
     </div>
   )
 }
