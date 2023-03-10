f62668bd226c296ce5db8a981e43f007c5ffeb28
William Flores <floreswilliam891@hotmail.com>
2023-03-09 19:56:31 -0500

    adding product page, edited css

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/pic1.jpg';
+import product2 from './assets/pic2.jpg';
+import product3 from './assets/pic3.jpg';
+import product4 from './assets/pic4.jpg';
+import product5 from './assets/pic5.jpg';
+import product6 from './assets/pic6.jpg';
+import product7 from './assets/pic7.jpg';
+import product8 from './assets/pic8.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Jordan 1 Chicago",
+        price: 400.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Jordan 4 Oreo",
+        price: 400.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Nike Air Max Sean Wotherspoon 97",
+        price: 400.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Jordan 5 Off White",
+        price: 400.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "LeBron 10s All Star",
+        price: 400.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Jordan 11 Playoffs",
+        price: 400.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Jordan 3 Cements",
+        price: 400.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Jordan 7 USA",
+        price: 400.0,
+        productImage: product8,
+    },
+]
